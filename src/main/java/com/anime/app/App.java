4be62024704c143b049c681006f104b49705337e
kuro <olegrumblerstain@yahoo.com>
2018-05-18 22:07:24 +0300

    Cleaned using Maven

--- src/main/java/com/anime/app/App.java
+++ src/main/java/com/anime/app/App.java
@@ -6,7 +6,7 @@ package com.anime.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] arg )
     {
         System.out.println( "Hello World!" );
     }
